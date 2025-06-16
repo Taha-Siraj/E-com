64d96d5cc639c11d9939cf4ed078ba0d6a1d9518
Taha Siraj <Pikachugaming899@gmail.com>
2025-06-16 11:01:05 -0700

    add ecom server

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,17 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ContextProvider from './Context/Context.jsx'
 import '@fontsource/poppins';
 
-const queryClient = new QueryClient();
 createRoot(document.getElementById('root')).render(
    
    <ContextProvider>
-   <QueryClientProvider client={queryClient}>
    <BrowserRouter>
    <App />
    </BrowserRouter>
-   </QueryClientProvider>
    </ContextProvider>
   
 )
