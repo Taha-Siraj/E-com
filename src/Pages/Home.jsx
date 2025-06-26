a2d85a5b95194e2f35c208d65d8e5a69a3e2983d
Taha Siraj <Pikachugaming899@gmail.com>
2025-06-26 08:58:04 -0700

    add new code

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { GlobalContext } from '../Context/Context';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { toast } from 'sonner';
 import axios from 'axios';
 import './home.css'
@@ -29,9 +29,21 @@ const Home = () => {
   };
 
   return (
-    <div id='home' className="font-poppins  flex items-center justify-center px-4">
+    <>
+ 
+    <div id='home' className="font-poppins">
+          <div className='h-[70px] flex bg-gray-400  justify-between px-10 items-center font-poppins top-0 left-0 right-0 sticky z-10' id='nav'>
+      <div className='h-full py-1'>
+          <img src="/logo.png" className='h-full'  alt="" />
+      </div>
+      <div className=' flex text-xl justify-center items-center gap-x-5'>
+        <Link to='/home' className='no-underline text-gray-200 hover:text-gray-600 transition duration-75' >Home</Link>
+        <Link to='/product' className='no-underline text-gray-200 hover:text-gray-600 transition duration-75 '>products</Link>
+      </div>
+    </div>
       <Products/>
     </div>
+    </>
   );
 };
 
--- src/Pages/NavBar.jsx
+++ src/Pages/NavBar.jsx
@@ -2,15 +2,9 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 const NavBar = () => {
   return (
-    <div className='h-[70px] flex bg-gray-400  justify-between px-10 items-center font-poppins top-0 left-0 right-0 sticky z-10' id='nav'>
-      <div className='h-full py-1'>
-          <img src="/logo.png" className='h-full'  alt="" />
-      </div>
-      <div className=' flex text-xl justify-center items-center gap-x-5'>
-        <Link to='/home' className='no-underline text-gray-200 hover:text-blue-400 transition duration-75' >Home</Link>
-        <Link to='/product' className='no-underline text-gray-200 hover:text-blue-400 transition duration-75 '>products</Link>
-      </div>
-    </div>
+   <>
+   
+   </>
   )
 }
 
