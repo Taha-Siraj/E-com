9652f515d260c5c87af295fffdebf7b507a6af6a
Taha Siraj <Pikachugaming899@gmail.com>
2025-06-28 09:39:36 -0700

    add new code

--- src/Pages/AddProducts.jsx
+++ src/Pages/AddProducts.jsx
@@ -1,16 +1,77 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { toast, Toaster } from 'sonner';
 
 const AddProducts = () => {
+  
+  const baseUrl = 'https://server-ecom-rho.vercel.app';
+  const [productform, setproductform] = useState({
+    productName: "",
+    price: "",
+    description: "",
+    categoryId: "",
+    productImg: "",
+  })
+  const [allcategories , setAllcategory] = useState([])
+  const handleChange = (e) => {
+    const {name , value} = e.target;
+    setproductform((prev) => ({
+      ...prev,
+      [name]: value
+    }))
+    console.log(name, value)
+  }
 
+  const fetchCategory = async () => {
+    try {
+      let res = await axios.get(`${baseUrl}/allcategories`);
+      setAllcategory(res.data)
+    } catch (error) {
+      toast.error('Something went wrong!');
+    }
+  }
+  useEffect(() => {
+    fetchCategory()
+  },[])
 
-
-  const inputStyle = 'border-[0.5px] py-3 px-4 rounded-md placeholder:text-gray-300 w-full border-[#dadada6e] bg-gray-700 outline-none focus:border-gray-300 transition-all duration-75'
+  const inputStyle = 'border-[0.5px] py-3 px-4 rounded-md placeholder:text-gray-300 w-full border-[#dadada6e] bg-gray-700 outline-none focus:border-gray-300 transition-all duration-75';
+  const titleStyles = "text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-emerald-400 to-green-500  text-center";
   return (
-    <div>
-      <form className='flex justify-center flex-col items-center h-[400px] w-[400px] bg-gray-900 text-[#fff]  font-poppins px-6 py-6'>
-        <h1>Add Product</h1>
-        <input type="text" placeholder='Product Name' className={inputStyle} />
-        <input type="text" placeholder='Product Name' className={inputStyle} />
+    <div className='bg-gray-950 h-screen flex justify-center items-center'>
+      <Toaster position="top-center" richColors />
+      <form className='flex justify-center flex-col border-[#dadada6e] items-center border-[0.5px] rounded-lg min:h-[400px] w-[400px] bg-gray-800 text-[#fff] gap-y-3 font-poppins px-6 py-6'>
+        <h1 className={titleStyles}>Add Product</h1>
+        <input
+        type="text"
+        name='productName'
+        onChange={handleChange}
+        placeholder='Product Name' className={inputStyle} />
+        <input
+         type="number"
+         name='price'
+        onChange={handleChange}
+         placeholder='Product price'
+         className={inputStyle} />
+        <input 
+         type="text" 
+         placeholder='Product description'
+         name='description'
+        onChange={handleChange}
+         className={inputStyle} />
+        <input 
+        type="text" 
+        name='productImg'
+        onChange={handleChange}
+        placeholder='Product Imgage URL' 
+        className={inputStyle} />
+        <select className={inputStyle} onChange={handleChange}>
+          <option disabled >Select A category</option>
+          {allcategories.map((cat) => (
+            <option key={cat.category_id} value={cat.category_id}> {cat.category_name} </option>
+          ))} 
+        </select>
+        <button 
+        className='bg-green-800 text-gray-300 font-semibold flex justify-center w-full rounded-lg py-2 px-4 items-center'>Add Product</button>
       </form>
     </div>
   )
