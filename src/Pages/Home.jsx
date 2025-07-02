9e7bc981af1388546f4c6027b7d5b9ac7c804a8f
Taha Siraj <Pikachugaming899@gmail.com>
2025-07-02 11:13:08 -0700

    add new code

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -51,17 +51,17 @@ const Home = () => {
   </div>
   </div>
   <main>
-  <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 md:gap-8 p-4'>
+  <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6 md:gap-3 p-4'>
    {HomeProducts.map((eachProduct) => (
-    <div key={eachProduct._id} className='bg-white rounded-md flex flex-col overflow-hidden border-[0.5px] border-[#e2e5f795]'>
+    <div key={eachProduct._id} className='bg-white w-[250px] rounded-md flex flex-col overflow-hidden border-[0.5px] border-[#e2e5f795]'>
       <div className='relative overflow-hidden'>
         <img src={eachProduct.product_img} alt={eachProduct.product_name} className='w-full h-56 object-cover transform group-hover:scale-110 transition-transform duration-300' />
         {eachProduct?.category_name && ( 
-                    <div className='absolute top-3 left-3 bg-indigo-600 text-white text-xs font-bold px-3 py-1 rounded-full shadow-md'> 
-                      {eachProduct.category_name}
-                    </div>
-                  )}
-        <div className='flex flex-col'>
+       <div className='absolute top-3 left-3 bg-indigo-600 text-white text-xs font-bold px-3 py-1rounded-full shadow-md'> 
+           {eachProduct.category_name}
+          </div>
+           )}
+        <div className='px-3 text-start justify-center items-start capitalize py-4 gap-y-3 flex flex-col'>
           <h2 className='text-lg font-semibold text-gray-800'>{eachProduct.product_name}</h2>
           <p className='text-gray-600'>Price: ${eachProduct.product_price}</p>
           <div className='flex items-center gap-1'>
@@ -71,17 +71,22 @@ const Home = () => {
               <MdOutlineStarPurple500 key={index} className='text-gray-300' />
             ))}
           </div>
-        </div>
-      </div>
-      <button 
+        <button 
         onClick={() => navigate(`/product/${eachProduct._id}`)}
-        className='bg-green-600 text-white px-4 py-2 rounded-md hover:bg-green-700 transition-colors'>
-        View Details
+        className='bg-[#063c28cc] mt-2 flex justify-center gap-x-2 items-center rounded-full py-2 px-4 font-semibold hover:scale-100 transition-all  text-[#f0f0f0]'><FaLuggageCart/>
+        Add To cart
       </button>
+        </div>
+        
+      </div>
+      
     </div>
     ))}
   </div>
   </main>
+
+
+
   <div className='px-10'>
     <div className='py-5 items-start px-10 border border-gray-300 rounded-md w-full'>
     <h1 className='text-2xl'>Popular Categories</h1>
