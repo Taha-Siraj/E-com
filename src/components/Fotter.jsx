4684fa47e7b86216cdb93831c5b54ee7e9f496da
Taha Siraj <Pikachugaming899@gmail.com>
2025-07-03 10:47:04 -0700

    add new code

--- src/components/Fotter.jsx
+++ src/components/Fotter.jsx
@@ -5,10 +5,33 @@ import { IoTimeOutline } from "react-icons/io5";
 import { MdOutlineEmail } from "react-icons/md";
 
 const Fotter = () => {
+
+  const contactInfo = [
+    {
+      icon: <IoLocationSharp className='text-2xl text-[#FF6B6B]' />,
+      title: 'Visit Us',
+      text: 'karachi, pakistan',
+    },
+    {
+      icon: <IoCallSharp className='text-2xl text-[#FF6B6B]' />,
+      title: 'Call Us',
+      text: '+92 3123456789',
+    },
+    {
+      icon: <IoTimeOutline className='text-2xl text-[#FF6B6B]' />,
+      title: 'Working Hours',
+      text: 'Mon - Fri: 9 AM - 5 PM',
+    },
+    {
+      icon: <MdOutlineEmail className='text-2xl text-[#FF6B6B]' />,
+      title: 'Email Us',
+      text: 'e-shop@gmail.com'
+    }        
+  ]
   return (
     <div>
-      <div>
         <hr />
+      <div>
       
       </div>
     </div>
