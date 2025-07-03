78ee53092e94dbee29e4ae75239f4e9a59952ae4
Taha Siraj <Pikachugaming899@gmail.com>
2025-07-03 09:58:46 -0700

    add new code

--- src/Blog/Allblogs.jsx
+++ src/Blog/Allblogs.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
 
 const Allblogs = () => {
+
+
+    const blogs = [{
+    img: 'lifestyle.webp',
+    title: 'Lifestyle',
+    desc: 'Office rental agency or direct? Which is best when renting an office?',
+    date: 'February 19, 2025', 
+  },
+  {
+    img: 'socialmedia.webp',
+    title: 'Social Media',
+    desc: 'Lotus Electronics – New Store Launch in Bhilai, Chhattisgarh',
+    date: 'February 19, 2025', 
+  },     
+  {
+    img: 'company.webp',
+    title: 'Company News',
+    desc: 'We Invite You to These Wonderful Wine Tasting Events',
+    date: 'February 19, 2025', 
+  },     
+  {
+    img: 'elctronics.webp',
+    title: 'Electronics',
+    desc: '10 French Wine Regions to Visit for Amazing Views and Delicious Vinos',
+    date: 'February 19, 2025', 
+  },     
+    ]
   return (
-    <div className='py-24'>
+    <div className='py-24 font-poppins px-10'>
+      <h1 className='text-2xl py-3'>Our Blogs</h1>
       <div>
-        <h1>Our Blogs</h1>
+        <div>  </div>
       </div>
     </div>
   )
