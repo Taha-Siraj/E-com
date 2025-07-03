22827b971691f2962fda8cfb452269ebdae62472
Taha Siraj <Pikachugaming899@gmail.com>
2025-07-03 10:35:53 -0700

    add new code

--- src/components/Fotter.jsx
+++ src/components/Fotter.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import { IoLocationSharp } from "react-icons/io5";
+import { IoCallSharp } from "react-icons/io5";
+import { IoTimeOutline } from "react-icons/io5";
+import { MdOutlineEmail } from "react-icons/md";
 
 const Fotter = () => {
   return (
