17494787d03b6fbc5d26f454a679c4dd97153462
Taha Siraj <Pikachugaming899@gmail.com>
2025-07-05 10:46:19 -0700

    add new code

--- src/Pages/Productsdetail.jsx
+++ src/Pages/Productsdetail.jsx
@@ -45,7 +45,7 @@ const ProductDetail = () => {
 
   return (
     <div className="mt-24 font-poppins px-16 py-2 flex flex-col gap-4 md:flex-row justify-between  items-start">
-      <Toaster position="bottom-right" richColors />
+      <Toaster position="top-right" richColors />
 
      <div className='h-[550px] w-full border p-5 rounded-lg flex justify-center items-center '>
        <img src={product.product_img} alt={product.product_name} className="w-full h-full object-center hover:scale-110 transition-all duration-300 cursor-pointer" />
