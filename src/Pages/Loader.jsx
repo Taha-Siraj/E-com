e29f0629236e2fdfaeee5400c3efbd8174254071
Taha Siraj <Pikachugaming899@gmail.com>
2025-07-05 06:52:01 -0700

    add new code

--- src/Pages/Loader.jsx
+++ src/Pages/Loader.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Loader = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen gap-4">
-  <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
-  <p className="text-blue-600 font-semibold animate-pulse">Loading, please wait...</p>
+  <div className="w-12 h-12 border-4 border-green-800 border-t-transparent rounded-full animate-spin"></div>
+  <p className="text-green-800 font-semibold animate-pulse">E-Shop is Loading, please wait...</p>
 </div>
 
   );
