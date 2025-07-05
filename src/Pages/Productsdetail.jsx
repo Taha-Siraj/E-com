73b1ecdce22803dd81b168f37d2dafc3a800d21e
Taha Siraj <Pikachugaming899@gmail.com>
2025-07-05 03:38:48 -0700

    add new code

--- src/Pages/Productsdetail.jsx
+++ src/Pages/Productsdetail.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { MdOutlineStarPurple500, MdStarPurple500 } from "react-icons/md";
 import { FaLuggageCart } from 'react-icons/fa';
+import { toast } from 'sonner';
 
 const ProductDetail = () => {
   const baseUrl = 'http://localhost:5004';
@@ -29,11 +30,16 @@ const ProductDetail = () => {
     fetchProduct();
   }, [id]);
 
+  const handleAddToCart = () => {
+    toast.success('Product added to cart successfully!');
+  }
+
   if (loading) return <p className="p-10">Loading...</p>;
   if (!product) return <p className="p-10 text-red-500">Product not found</p>;
 
   return (
     <div className="mt-24 font-poppins px-16 py-2 flex flex-col gap-4 md:flex-row justify-between  items-start">
+      
      <div className='h-[550px] w-full border p-5 rounded-lg flex justify-center items-center '>
        <img src={product.product_img} alt={product.product_name} className="w-full h-full object-center hover:scale-110 transition-all duration-300 cursor-pointer" />
      </div>
@@ -53,7 +59,10 @@ const ProductDetail = () => {
       <p className="text-lg text-green-800 font-bold">Rs. {product.price}.00</p>
       <span className='text-green-700  font-semibold capitalize bg-green-100 py-[2px] rounded-lg px-2'>in Stock</span>
       <hr className='bg-gray-100 h-[0.1px] w-full'/>
-      <button className='bg-[#063222cc] w-full flex justify-center gap-x-2 items-center rounded-lg py-2 px-4 font-semibold hover:bg-[#063c28] transition-all  text-[#f0f0f0]'><FaLuggageCart/>
+      <button 
+      onClick={handleAddToCart}
+      className='bg-[#063222cc] w-full flex justify-center gap-x-2 items-center rounded-lg py-2 px-4 font-semibold hover:bg-[#063c28] transition-all  text-[#f0f0f0]'><FaLuggageCart/>
+
       Add To cart
       </button>
       </div>
